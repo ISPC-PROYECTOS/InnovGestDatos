bb34a2563518f5341d33d91b41c38015929e850a
PereyraHebe <p_hebe_c@hotmail.com>
2024-10-30 09:36:58 -0300

    correccion manejo de excepcion con la coneccion BBDD, para que menu se siga ejecutando sin coneccion a BBDD

--- proyecto_final/BDII/conexion_bd.py
+++ proyecto_final/BDII/conexion_bd.py
@@ -1,46 +1,33 @@
-"""import mysql.connector
 
-HOST = "localhost"
-USER = "root"
-PASSWORD = ""
-BD = "stockagranel"
-
-conn = mysql.connector.connect(
-    host=HOST,
-    user=USER,
-    password=PASSWORD,
-    database=BD
-)
-
-cursor = conn.cursor()
-
-def cerrarConexion():
-    cursor.close()
-    conn.close()"""
+#------------------------------------prueba raice------------------------
 import mysql.connector
 from mysql.connector import Error
 
 HOST = "localhost"
 USER = "root"
-PASSWORD = "tu_contraseña"
+PASSWORD = "su_contraseña"
 BD = "stockagranel"
 
-try:
-    conn = mysql.connector.connect(
-        host=HOST,
-        user=USER,
-        password=PASSWORD,
-        database=BD
-    )
-    if conn.is_connected():
-        print("Conexión a la base de datos exitosa.")
-        cursor = conn.cursor()
-except Error as e:
-    print(f"Error al conectar a la base de datos: {e}")
-    print("Por favor, configure los datos de conexión y pruebe nuevamente.")
+class ErrorConexion(Exception):
+    pass
 
-def cerrarConexion():
+def conectar_base_datos():
+    try:
+        conn = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=BD
+        )
+        if conn.is_connected():
+            print("Conexión a la base de datos exitosa.")
+            return conn
+    except Error as e:
+        raise ErrorConexion(f"Error al conectar a la base de datos: {e}")
+
+def cerrar_conexion(conn):
     if conn.is_connected():
-        cursor.close()
+        conn.cursor().close()
         conn.close()
         print("Conexión a la base de datos cerrada.")
+
--- proyecto_final/PROGI/main.py
+++ proyecto_final/PROGI/main.py
@@ -1,37 +1,41 @@
+#----------------------------------------------------prueba raice------------------------
 import sys
 import os
-
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from modulos_main import menu_usuarios
 from inicio_sesion import iniciar_sesion
 from registros_pluviales import menu_registros_pluviales
-#from BDII import index
-
+from BDII.conexion_bd import conectar_base_datos, ErrorConexion, cerrar_conexion
 
 def main():
     salir = False
-
     while not salir:
         print('-----> MENÚ PRINCIPAL <-----')
         print('1. USUARIOS Y ACCESOS') #menu_usuarios
         print('2. INICIAR SESIÓN') #menu_stock_granel
         print('3. ANÁLISIS DE DATOS')
         print('4. SALIR')
-
         opcion = input("Seleccione una opción: ")
-            
+
         if opcion == '1':
             menu_usuarios()
         elif opcion == '2':
-            sesion_iniciada=iniciar_sesion()
-             
-            if sesion_iniciada is True:                                 
-                from BDII import index
-                index.menu_stock_granel()
-
-
+            try:
+                sesion_iniciada = iniciar_sesion()
+                conn = conectar_base_datos()
+                
+                if sesion_iniciada is True:
+                    from BDII import index
+                    index.menu_stock_granel()
+                cerrar_conexion(conn)
+            except ErrorConexion as e:
+                print(e)
+                print("Por favor, configure los datos de conexión y pruebe nuevamente.")
+                opcion = input("¿Deseas volver al menú principal? (s/n): ")
+                if opcion.lower() == 's':
+                    continue
+                else:
+                    salir = True
         elif opcion == '3':
             menu_registros_pluviales()
         elif opcion == '4':
